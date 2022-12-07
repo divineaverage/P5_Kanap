68a4b70a0de7c890df1c9a5f2cee887ffde1a547
Lindsay J <lindsay.jawor@gmail.com>
2022-12-07 12:28:12 -0500

    pictures and titles are live with javascript

--- front/js/script.js
+++ front/js/script.js
@@ -1,19 +1,24 @@
 async function fetchProducts() {
     const response = await fetch('http://localhost:3000/api/products');
-    const products = await response.json();
-    if (typeof data === 'undefined') {
-        return alert ('something is wrong!')
+    return response.json();
     }
-}
+
+// Get DOM elements
+const productContainer = document.getElementById ('items');
+
 
 fetchProducts().then(products => {
-    products;
+    for (let product of products) {
+        const productElement = `<a href="./product.html?id=${product._id}">
+        <article>
+          <img src="${product.imageUrl}" alt="${product.altText}">
+          <h3 class="productName">${product.name}</h3>
+          <p class="productDescription">${product.description}</p>
+        </article>
+      </a>`;
+        productContainer.innerHTML += productElement
+    }
 });
 
-const article = document.getElementsByClassName('article');
-const link = document.getElementsByClassName('a');
-const image = document.getElementsByClassName('img');
-const productName = document.getElementsByClassName('productName');
-const productDescription = document.getElementsByClassName ('productDescription');
 
-apiRequest.open('GET', 'http://localhost:3000/api/products'); apiRequest.send();
+
