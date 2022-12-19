459a3f38cd15123622fb6735fc71508dde6e2b31
Lindsay J <lindsay.jawor@gmail.com>
2022-12-19 14:49:12 -0500

    updates from mentor session to fix local storage and cart

--- front/js/product.js
+++ front/js/product.js
@@ -1,39 +1,36 @@
-
 // Get product ID //
 const getProductId = () => {
   return new URL(location.href).searchParams.get("id");
 };
 const productId = getProductId();
 
-fetch(`http://localhost:3000/api/products/${productId}`)
-  .then((response) => {
-    return response.json();
-  })
+function init() {
+  fetch(`http://localhost:3000/api/products/${productId}`)
+    .then((response) => {
+      return response.json();
+    })
 
-  .then((product) => {
-    productSelection(product);
-    productDetails(product);
-  })
-  .catch((error) => {
-    alert(error);
-  });
-
-// Fill html //
-const selectedColor = document.querySelector("#colors");
-const selectedQuantity = document.querySelector("#quantity");
-const button = document.querySelector("#addToCart");
+    .then((product) => {
+      productSelection(product);
+      productDetails(product);
+    })
+    .catch((error) => {
+      alert(error);
+    });
+}
 
 // Functions //
 let productSelection = (product) => {
-  
   document.querySelector("head > title").textContent = product.name;
-  document.querySelector(".item__img")
-  .innerHTML += `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
+  document.querySelector(
+    ".item__img"
+  ).innerHTML += `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
   document.querySelector("#title").textContent += product.name;
   document.querySelector("#price").textContent += product.price;
   document.querySelector("#description").textContent += product.description;
+  const selectedColor = document.querySelector("#colors");
 
-   for (color of product.colors) {
+  for (color of product.colors) {
     let option = document.createElement("option");
     option.innerHTML = `${color}`;
     option.value = `${color}`;
@@ -41,21 +38,25 @@ let productSelection = (product) => {
   }
 };
 
-// Cart function //
+// User functionality //
 function productDetails(product) {
+  const selectedQuantity = document.querySelector("#quantity");
+  const button = document.querySelector("#addToCart");
+  const selectedColor = document.querySelector("#colors");
 
   button.addEventListener("click", (event) => {
     event.preventDefault();
 
     if (selectedColor.value == false) {
-      confirm("Please select a color");
+      alert("Please select a color");
     } else if (selectedQuantity.value == 0) {
-      confirm("Please select a quantity");
+      alert("Please select a quantity");
     } else {
       alert("Added to cart");
+    }
 
-// Get product details //
-    let productSelection = {
+    // Get product details //
+    let userSelection = {
       id: product._id,
       name: product.name,
       img: product.imageUrl,
@@ -64,3 +65,26 @@ function productDetails(product) {
       color: selectedColor.value,
       quantity: parseInt(selectedQuantity.value, 10),
     };
+
+    // Local Storage //
+    let previousCart = localStorage.getItem("cart")
+      ? JSON.parse(localStorage.getItem("cart"))
+      : [];
+    let item = previousCart.find(
+      (item) =>
+        item.id === userSelection.id && userSelection.color === item.color
+    );
+    if (item) {
+      const itemIndex = previousCart.findIndex(
+        (item) =>
+          item.id === userSelection.id && userSelection.color === item.color
+      );
+      previousCart.splice(itemIndex);
+      userSelection.quantity += item.quantity;
+    }
+    previousCart.push(userSelection);
+    localStorage.setItem("cart", JSON.stringify(previousCart));
+  });
+}
+
+init();
